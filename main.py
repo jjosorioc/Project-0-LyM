5153c9fffd379623b7c09acd699b283ade255600
larubiano0 <larubiano0@gmail.com>
2021-08-27 21:03:39 -0500

    Advancement on the AI Machine Learning methods

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ Project 0
 - Juan José Osorio (@jjosorioc)
 - Luis Rubiano (@larubiano)
 """
+# SETTINGS
 
+file_name = "commands.txt" #Name of the text file 
+
+# FUNCTIONS
+
+def openFile(file_name: str):
+    with open("commands.txt", "r") as cmdFile:
+        lines = cmdFile.readlines()
+        lines.append("\n")
+    return lines
 
 
 def filterByCommand(lines: list[str])->list[str]:
@@ -17,9 +27,7 @@ def filterByCommand(lines: list[str])->list[str]:
     """
     indice = 0 # First index of 'lines'
     indiceDelComando = indice + 1 # Second index of 'lines' // It's the last index of each command.
-    
     nuevaLista = [] # List the function will return
-    
     while (indiceDelComando < len(lines)):
 
         if (lines[indiceDelComando] == "\n"):
@@ -45,51 +53,50 @@ def filterByCommand(lines: list[str])->list[str]:
         
     return nuevaLista
 
+#####def addVariable():
 
+#####def defineFunction():
 
+# DATA STRUCTURES
 
-with open("commands.txt", "r") as cmdFile:
-
-    lines = cmdFile.readlines()
-    lines.append("\n")
-
-
-    
-    print(filterByCommand(lines))
-
-
-    lista_comandos = {
-        'MOVE': 'int',
-        'RIGHT': 'int',
-        'LEFT': 'int',
-        'ROTATE': 'int',
-        'LOOK': [ # Lista por las coordenadas north east south west
+command_dictionary = {
+        'MOVE': int,
+        'RIGHT': int,
+        'LEFT': int,
+        'ROTATE': int,
+        'LOOK': [ # Coordinate list north east south west
             'N',
             'E',
             'W',
             'S'
         ],
-        "DROP": 'int',
-        "FREE": 'int',
-        "PICK": 'int',
-        "POP": 'int',
-        "CHECK": (['C', 'B'], 'int'), # tuple -> (list, str)
-        "BLOCKEDP": None, #es un boolean
+        "DROP": int,
+        "FREE": int,
+        "PICK": int,
+        "POP": int,
+        "CHECK": (['C', 'B'], int), # tuple -> (list, int)
+        "BLOCKEDP": None, #is a bool
         "NOP": None, #Robot doesn't do shit
-    }
-    
-    #     Falta esto:
-    #     A block of commands: (BLOCK commands) where commands is simply a se-
-    # quence of one or more commands (separated by new lines).
-    # { Iterative instructions: (REPEAT n [commands]), where n is a variable or a num-
-    # bers describen the number of times the commands inside the [] will repeat,
-    # and commands is a sequence of basic commands separated by new lines.
-    # { A conditional command: IF expr [ commands ], where expr is a boolean ex-
-    # pression, and commands is a sequence of basic commands separated by new
-    # lines
-    # { DEFINE n val { denes a new variable n assigning it value val (an integer).
-    # Note that variable names need to be lowercase.
-    # { TO f :param OUTPUT expression END. Functions are dened between the TO
-    # and END keywords, giving them a name f and a list of space separated parame-
-    # ters each dened by the colons before its name (as in :param). The inner works
-    # of a function are given as an expression or block of commands in its OUTPUT.
+        "BLOCK": [], #All the possible commands that can be after a "BLOCK" statement
+        "REPEAT" : (int, []), # tuple -> (int, command_list)
+}
+
+command_list = list(command_dictionary.keys()) #Python list of all the posible commands
+command_dictionary["BLOCK"]=command_list
+command_dictionary["REPEAT"][1]=command_list
+
+
+variables = {} #Python dictionary of all posible user created variables
+
+
+
+
+# EXECUTION
+lines = openFile(file_name)
+print(lines)
+
+
+
+
+# DEV COMMENTS
+    ##########Verificar recursión
