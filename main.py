3885d878b743d686746629f0b055649203e0dc09
larubiano0 <larubiano0@gmail.com>
2021-09-04 05:00:48 -0500

    Parser almost done

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Project 0
 
 
 import os
-FILE_NAME = "commands.txt" #Name of the text file 
+FILE_NAME = "deletelater.txt" #Name of the text file 
 
 
 ##################################### CONSTANTS ##################################### 
@@ -143,7 +143,7 @@ def readLineByLine(lines: list[str], localVars = None):
 
     Args:
         lista (list[str]): List by each token.
-        localVars (dict): Local variables of a function, by default none, can be used in recursion cases.  
+        localVars (list): Local variables of a function, by default none, can be used in recursion cases.  
 
     Returns:
         (None)
@@ -152,8 +152,10 @@ def readLineByLine(lines: list[str], localVars = None):
 
     while countA<len(lines):
         
-
+        
         if lines[countA] not in COMMAND_DICTIONARY:
+            print(lines[countA])
+            print(COMMAND_DICTIONARY)
             raise Exception("\n"*5 + "ERROR: Undefined command " + lines[countA] + "\n"*5)
         
 
@@ -165,49 +167,62 @@ def readLineByLine(lines: list[str], localVars = None):
             command += lines[countA]
 
 
-        if type(COMMAND_DICTIONARY[command]) == tuple:
+        if command == "(BLOCK":
+            recursiveLines = []
+            while lines[countA+1]!=")":
+                countA += 1
+                recursiveLines.append(lines[countA])
+            countA += 1
+            readLineByLine(recursiveLines)
 
+        elif command == "(REPEAT":
+            pass
+
+        elif command == "TO":
+            split_list = []
+            split_list.append(lines[countA])
+            while lines[countA]!="END":
+                countA += 1
+                split_list.append(lines[countA])
+            recursiveLines, localVars = defineFunction(split_list)
+            readLineByLine(recursiveLines,localVars)
+
+        elif command == "IF":
+            countA += 1
+            if not lines[countA] in COMMAND_DICTIONARY[command][0]:
+                raise Exception("\n"*5 + "ERROR: Argument " + lines[countA] + " is incorrect" + "\n"*5)
+            countA += 1
+            if lines[countA] != '[':
+                raise Exception("\n"*5 + "ERROR: Missing [ on if command" + "\n"*5)
+            countA += 1
+            recursiveLines = [] 
+            while lines[countA+1]!="]":
+                recursiveLines.append(lines[countA])
+                countA += 1
+            countA += 1
+            readLineByLine(recursiveLines)
+            
+        elif type(COMMAND_DICTIONARY[command]) == None:
+            pass #Robot ain't doin' shit
+    
+        else:
             countB = 0 #Current argument within command
             
             while countB < len(COMMAND_DICTIONARY[command]):
-
                 countA += 1
-
                 if COMMAND_DICTIONARY[command][countB] == "int":
-                    assertIsAnInteger(lines[countA])
-
+                    assertIsAnInteger(lines[countA],localVars)
                 elif COMMAND_DICTIONARY[command][countB] == 'variable_name':
-                    addVariableIfInteger(COMMAND_DICTIONARY[command][0],COMMAND_DICTIONARY[command][1])
-
+                    addVariableIfInteger(lines[countA],lines[countA+1])
                 elif COMMAND_DICTIONARY[command][countB] == 'error':
                     raise Exception("\n"*5 + "ERROR: Syntax error" + "\n"*5)
-
                 elif type(COMMAND_DICTIONARY[command][countB]) == list:
                     if not lines[countA] in COMMAND_DICTIONARY[command][countB]:
                         raise Exception("\n"*5 + "ERROR: Argument " + lines[countA] + " is incorrect" + "\n"*5)
-
-                elif COMMAND_DICTIONARY[command][countB] == '[':
-                    pass#TODO
-
-                elif COMMAND_DICTIONARY[command][countB] == 'f':
-                    pass#USARFUNCIONES
-
-                elif COMMAND_DICTIONARY[command][countB] == ':param':
-                    pass#TODO
-
-                elif COMMAND_DICTIONARY[command][countB] == 'OUTPUT':
-                    pass#TODO
-            
-                elif COMMAND_DICTIONARY[command][countB] == 'commands':
-                    pass#TODO
-
                 countB += 1
 
-        elif type(COMMAND_DICTIONARY[command]) == None:
-            pass #Robot ain't doin' shit
-
-
         countA += 1
+
 
 def verifyIsInAlphabet(sequence_of_symbols, alphabet)->bool:
     """Function to verify a sequence of symbols is over a given alphabet.
@@ -239,17 +254,22 @@ def verifyNameIsNotRestricted(name):
         return False
 
 
-def assertIsAnInteger(n):
+def assertIsAnInteger(n, localVars = None):
     """Function to verify a parameter is either an integer or a user-saved integer variable.
     
     Args:
         n: An integer or a user-saved integer variable.
+        localVars (list): Local variables of a function, by default none, can be used in recursion cases.  
 
     Returns:
         (None)
     """
-    if not (verifyIsInAlphabet(n, BASE_TEN_NUMBERS_ALPHABET) or (n in VARIABLE_DICTIONARY)):
-        raise Exception("\n"*5 + "ERROR: " + n + " variable is undefined" + "\n"*5)
+    if type(localVars)!=None:
+        if not (verifyIsInAlphabet(n, BASE_TEN_NUMBERS_ALPHABET) or (n in VARIABLE_DICTIONARY) or (n in localVars)):
+            raise Exception("\n"*5 + "ERROR: " + n + " variable is undefined" + "\n"*5)
+    else:
+        if not (verifyIsInAlphabet(n, BASE_TEN_NUMBERS_ALPHABET) or (n in VARIABLE_DICTIONARY)):
+            raise Exception("\n"*5 + "ERROR: " + n + " variable is undefined" + "\n"*5)
 
 
 def addCommand(name, args):
@@ -280,17 +300,17 @@ def addVariableIfInteger(name, value):
     else: raise Exception("\n"*5 + "ERROR: Syntax error regarding " + name + " variable" + "\n"*5)
 
 
-def defineFunction(TO_command):
+def defineFunction(split_list:list[str])->list[str]:
     """Function to define a new TO command.
     Args:
-        TO_command: Python string with the whole TO function.
+        split_list (list[str]): Python list with the whole TO function.
     Returns:
         list[str]: The expression inside the function to be asserted afterwards
     """
-    split_list = TO_command.split(" ")
     name = split_list[1]
+    localVars = []
     if not (split_list[0]=="TO" and split_list[-1]=="END" and ("OUTPUT" in split_list)):
-        raise Exception("\n"*5 + "ERROR: Wrong grammar for the function: " + name + "\n"*5)
+        raise Exception("\n"*5 + "ERROR: Wrong syntax for the function: " + name + "\n"*5)
     if not (verifyIsInAlphabet(name, LOWERCASE_ALPHABET) and verifyNameIsNotRestricted(name)): 
         raise Exception("\n"*5 + "ERROR: Wrong name for the function: " + name + "\n"*5)
     arguments = []
@@ -300,10 +320,19 @@ def defineFunction(TO_command):
             break
         if split_list[count][0] != ":":
             raise Exception("\n"*5 + "ERROR: Wrong argument syntax for the function: " + name + "\n"*5)
-        if not (verifyIsInAlphabet(split_list[count][1:], LOWERCASE_ALPHABET) and verifyNameIsNotRestricted(name)): 
+        if not (verifyIsInAlphabet(split_list[count][1:], LOWERCASE_ALPHABET) and verifyNameIsNotRestricted(split_list[count][1:])): 
             raise Exception("\n"*5 + "ERROR: Wrong name for the argument: " + split_list[count][1:] + " in the function: " + name + "\n"*5)
         arguments.append(split_list[count][1:])
+        localVars.append(split_list[count])
+        count+=1
+    arguments = tuple(arguments)
     addCommand(name,arguments)
+    count+=1
+    recursiveLines = []
+    while split_list[count]!="END":
+        recursiveLines.append(split_list[count])
+        count += 1
+    return (recursiveLines,localVars)
 
 
 ##################################### EXECUTION #####################################
@@ -321,8 +350,8 @@ if __name__ == "__main__":
 
 ##################################### DEBUG #####################################
 
-#for i in tokenList:
-#    print(i)
+#TODO: Casos en los que se inicia un block, funcion, etc, pero nunca se acaba
+#Repeat function
 
 """
 ROTATE
